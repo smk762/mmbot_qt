6ef3f9a1dc4a0b3f10768430a6cf131ca24889ce
smk762 <smk762@iinet.net.au>
2019-11-04 06:40:46 +0800

    activation selection

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -11,6 +11,33 @@ class Ui(QTabWidget):
         super(Ui, self).__init__() # Call the inherited classes __init__ method
         uic.loadUi('ui/gui_template.ui', self) # Load the .ui file
         self.show() # Show the GUI
+    def activate_coins(self):
+        activate_dict = {}
+        coins = {
+            "BTC": {
+                "checkbox": self.checkBox_btc, 
+                "combo": self.btc_combo,
+            },
+            "ETH": {
+                "checkbox": self.checkBox_eth, 
+                "combo": self.eth_combo,
+            },
+            "KMD": {
+                "checkbox": self.checkBox_kmd, 
+                "combo": self.kmd_combo,
+            },
+            "LABS": {
+                "checkbox": self.checkBox_labs, 
+                "combo": self.labs_combo,
+            }
+        }
+        for coin in coins:
+            checkbox = coins[coin]['checkbox']
+            combo = coins[coin]['combo']
+            if checkbox.isChecked():
+                activate_dict.update({coin:combo.currentText()})
+        print(activate_dict)
+            
 app = QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
 window = Ui() # Create an instance of our class
 app.exec_() # Start the application
